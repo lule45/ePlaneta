5f296f7cfbbb0677b35cad02730fdc2875af8953
Luka Gajic <luka.gajic45@gmail.com>
2024-03-21 15:26:19 +0100

    na BaseTest klasi promenio metodu setup, tako da se opciono otvara hrom, ali moze i nesto drugo

--- src/test/java/tests/FailedLoginTest.java
+++ src/test/java/tests/FailedLoginTest.java
@@ -10,20 +10,17 @@ import pages.LoginPage;
 
 public class FailedLoginTest extends BaseTest {
     @Test
-    public void failedLogin() throws InterruptedException {
+    public void failedLogin() {
         SoftAssert softAssert = new SoftAssert();
         LoginPage loginPage = new LoginPage(driver);
 
         loginPage.clickOnProfileButton();
 
-        WebElement loginContainer = driver.findElement(By.xpath("//div [@class = 'block block-social-login']"));
         if (driver.findElement(By.id("block-customer-heading")).getText().contains("Kreiraj nalog")) {
             driver.findElement(By.xpath("//p [@id = 'login-register-btn']")).click();
         } else {
             loginPage.performLogin();
         }
-
-        Thread.sleep(3000);
 
         softAssert.assertEquals(driver.getCurrentUrl(), "https://eplaneta.rs/customer/account/login", "Asertacija pukla kod kredencijala");
         softAssert.assertEquals(driver.findElement(By.xpath("//div [text()= 'Neuspešna prijava ili je tvoj nalog ugašen. Pokušaj ponovo.']"))
