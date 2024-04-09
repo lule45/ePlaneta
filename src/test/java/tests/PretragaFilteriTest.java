2d2390826badcc32be7e2b2e27048848c2f49dc0
Luka Gajic <luka.gajic45@gmail.com>
2024-04-09 15:12:13 +0200

    popravljen test za PretragaFilteriTest.java moralo je da se ubaci jos 2 dodatne liste jer su cene bile pod drugacijim lokatorima (special price, normal special price, i regular price)

--- src/test/java/tests/PretragaFilteriTest.java
+++ src/test/java/tests/PretragaFilteriTest.java
@@ -2,19 +2,25 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.BasePage;
+import pages.HomePage;
 
+import javax.swing.*;
 import java.util.List;
 
 public class PretragaFilteriTest extends BaseTest {
+    By priceLocatorBy = By.xpath("//span [@class = 'price']");
 
     @Test
     public void pretragaFilteriTest() {
 
         BasePage basePage = new BasePage(driver);
         basePage.searchForSomething("nike");
+        SoftAssert softAssert = new SoftAssert();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://eplaneta.rs/catalogsearch/result/?q=nike");
 
@@ -29,17 +35,41 @@ public class PretragaFilteriTest extends BaseTest {
 
         basePage.waitForLoader();
 
-        Assert.assertEquals(driver.getCurrentUrl(), "https://eplaneta.rs/catalogsearch/result/index/?cat=4081&price=2000.00-5000.00&q=nike");
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://eplaneta.rs/catalogsearch/result/index/?cat=4081&price=2000.00-5000.00&q=nike");
 
-        List<WebElement> priceFiltersList = driver.findElements(By.xpath("//span[@class='normal-price special-price']"));
-        priceFiltersList.forEach(e -> {
-            String priceAsString = e.getText().replace(",99 RSD", "");
+        WebElement itemsContainer = driver.findElement(By.xpath("//ol[@class = 'products list items product-items']"));
 
-            if (!priceAsString.isBlank()) {
-                double priceAsDouble = Double.parseDouble(priceAsString.replace(".", ""));
+        List<WebElement> itemsList = itemsContainer.findElements(By.xpath(".//span [@class = 'special-price']"));
+        itemsList.forEach(e -> {
+            String priceAsString = e.getText().replace(".", "").substring(0, 4);
 
-                Assert.assertTrue(priceAsDouble < 5000 && priceAsDouble > 2000);
-            }
+            System.out.println(priceAsString + " SPECIAL");
+            double priceAsDouble = Double.parseDouble(priceAsString);
+            softAssert.assertTrue(priceAsDouble < 5000 && priceAsDouble > 2000);
+
         });
+
+        List<WebElement> itemsList2 = itemsContainer.findElements(By.xpath(".//span [@class = 'normal-price special-price ']"));
+        itemsList2.forEach(e -> {
+            String priceAsString = e.getText().replace(".", "").substring(0, 4);
+
+            System.out.println(priceAsString + " NORMAL SPECIAL");
+
+            double priceAsDouble = Double.parseDouble(priceAsString);
+            softAssert.assertTrue(priceAsDouble < 5000 && priceAsDouble > 2000);
+
+        });
+
+        List<WebElement> itemsList3 = itemsContainer.findElements(By.xpath(".//span [@class = 'normal-price regular ']"));
+        itemsList3.forEach(e -> {
+            String priceAsString = e.getText().replace(".", "").substring(0, 4);
+
+            System.out.println(priceAsString + " REGULAR");
+
+            double priceAsDouble = Double.parseDouble(priceAsString);
+            softAssert.assertTrue(priceAsDouble < 5000 && priceAsDouble > 2000);
+
+        });
+        softAssert.assertAll();
     }
 }
